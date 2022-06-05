a2733ef3d2fe0593c5bee6051611c3b60e3c9492
Yahor Fadzin <yahor_fadzin@epam.com>
2022-06-05 14:13:38 +0300

    fix comments

---
+++ config/webpack.dev.js
@@ -0,0 +1,15 @@
+const { merge } = require("webpack-merge")
+const common = require('./webpack.common.js')
+
+module.exports = merge(common, {
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    client: {
+      overlay: {
+        warnings: false,
+        errors: true,
+      },
+    },
+  },
+})
